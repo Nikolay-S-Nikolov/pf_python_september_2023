d6b779a19f72e82195348faf82d76ac37cf15090
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2023-11-04 15:31:01 +0200

    Exercise Dictionaries

---
+++ 25_dictionaries/00_02_test.py
@@ -0,0 +1,5 @@
+keys = ["name", "age", "major"]
+values = ["Ivan", 33, 'Computer Science']
+
+student = dict(zip(keys,values))
+print(student)
---
+++ 25_dictionaries/00_03_test.py
@@ -0,0 +1,13 @@
+my_dict = {"name": "Plamen", "age": 44, "occupation": "Engineer"}
+
+name = my_dict.get("name")
+name_1 = my_dict["name"]
+age = my_dict.get("age")
+age_1 = my_dict["age"]
+phone = my_dict.get("phone")
+
+print(name)
+print(name_1)
+print(age)
+print(age_1)
+print(phone)
