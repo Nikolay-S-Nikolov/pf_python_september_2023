85c73bdfda6cadadaa410f366c5191a2535a9349
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2023-11-22 17:10:04 +0200

    Problem 05. Legendary Farming

--- 26_exercise_dictionaries/05_legendary_farming.py
+++ 26_exercise_dictionaries/05_legendary_farming.py
@@ -1,9 +1,8 @@
 items_dict = {"shards": 0, "fragments": 0, "motes": 0}
-current_items = input().split()
 legendary_item = ""
-legendary = False
 
-while not legendary:
+while True:
+    current_items = input().split()
     for index in range(0, len(current_items), 2):
         value = int(current_items[index])
         key = current_items[index + 1].lower()
@@ -12,22 +11,18 @@ while not legendary:
         items_dict[key] += value
         if items_dict["shards"] >= 250:
             items_dict["shards"] -= 250
-            legendary = True
             legendary_item = "Shadowmourne"
+            break
         elif items_dict["fragments"] >= 250:
             items_dict["fragments"] -= 250
-            legendary = True
             legendary_item = "Valanyr"
+            break
         elif items_dict["motes"] >= 250:
             items_dict["motes"] -= 250
-            legendary = True
             legendary_item = "Dragonwrath"
-        if legendary:
             break
-    if legendary:
+    if legendary_item:
         break
-    current_items = input().split()
-
 print(f"{legendary_item} obtained!")
 for material, quantity in items_dict.items():
     print(f"{material}: {quantity}")
