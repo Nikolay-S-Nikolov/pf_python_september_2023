9a855d2585abf64d611e002fa6df5106a19416f9
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2023-11-22 11:27:03 +0200

    Problem 03. Capitals

---
+++ 26_exercise_dictionaries/03_capitals.py
@@ -0,0 +1,6 @@
+countries = input().split(", ")
+capitals = input().split(", ")
+# final_dict = {countries[index]: capitals[index] for index in range(len(capitals))}
+final_dict = dict(zip(countries, capitals))
+for country, capital in final_dict.items():
+    print(f"{country} -> {capital}")
