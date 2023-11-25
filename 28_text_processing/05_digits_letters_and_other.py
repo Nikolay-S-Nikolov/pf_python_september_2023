71651c5134d593d350e29e31cb127b7ed61ea4cb
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2023-11-25 20:02:55 +0200

    Problem 05. Digits, Letters and Other

---
+++ 28_text_processing/05_digits_letters_and_other.py
@@ -0,0 +1,16 @@
+single_string = input()
+
+digits = ""
+letters = ''
+other = ''
+
+for ch in single_string:
+    if ch.isdigit():
+        digits += ch
+    elif ch.isalpha():
+        letters += ch
+    else:
+        other += ch
+print(digits)
+print(letters)
+print(other)
