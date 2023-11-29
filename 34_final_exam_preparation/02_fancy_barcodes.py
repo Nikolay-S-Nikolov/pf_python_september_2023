884ee862a9df05367818f4658117213b1ecd95c4
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2023-11-29 11:52:25 +0200

    Problem 02. Fancy Barcodes

---
+++ 34_final_exam_preparation/02_fancy_barcodes.py
@@ -0,0 +1,22 @@
+import re
+
+count_barcode = int(input())
+
+pattern = r"@[#]+[A-Z]{1}[A-Za-z0-9]{3}[A-Za-z0-9]+[A-Z]{1}@[#]+"
+pattern_group = r'\d+'
+
+for _ in range(count_barcode):
+    barcode = input()
+    valid_code = re.findall(pattern, barcode)
+
+    if valid_code:
+        product_group = re.findall(pattern_group, barcode)
+        if not product_group:
+            x = '00'
+        else:
+            x = ""
+            for i in product_group:
+                x += i
+        print(f"Product group: {x}")
+    else:
+        print("Invalid barcode")
